8a5d4fb6a403d4a3fb49d424f2a8354810451ea6
riskipradana <riskipradana.rp7@gmail.com>
2018-09-22 23:12:37 +0700

    initial commit

---
+++ src/main/java/com/in28minutes/springboot/basics/springbootin10steps/BooksController.java
@@ -0,0 +1,20 @@
+package com.in28minutes.springboot.basics.springbootin10steps;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BooksController {
+
+    @GetMapping("/books")
+    public List<Book> getAllBooks(){
+        return Arrays
+                .asList(
+                        new Book(1L, "Mastering Spring 5.2", "Author")
+                );
+    }
+
+}
---
+++ src/main/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -0,0 +1,18 @@
+package com.in28minutes.springboot.basics.springbootin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringbootIn10StepsApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext applicationContext =
+				SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+
+		for (String name : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
+	}
+}
